d71a21ba10cd0274a3cae5320c8f7d1af48fdbbe
Steven Lu <stevenlu443@gmail.com>
2013-06-25 15:55:33 -0400

    Removing the debug now that I have seen it properly can tell what's going on with backspace

--- windows/window.c
+++ windows/window.c
@@ -4002,11 +4002,7 @@ static int TranslateKey(UINT message, WPARAM wParam, LPARAM lParam,
     shift_state = ((keystate[VK_SHIFT] & 0x80) != 0)
 	+ ((keystate[VK_CONTROL] & 0x80) != 0) * 2;
 
-	char buf[512];
-	sprintf(buf, "A key is pressed: %d with shift state %d", wParam, shift_state);
-	MessageBox(NULL, buf, "title?", 0);
-
-	/* Note if AltGr was pressed and if it was used as a compose key */
+    /* Note if AltGr was pressed and if it was used as a compose key */
     if (!compose_state) {
 	compose_key = 0x100;
 	if (cfg.compose_key) {
