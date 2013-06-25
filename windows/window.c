9183a57796406605d5c1317c79bc485910ca8702
Steven Lu <stevenlu443@gmail.com>
2013-06-25 16:12:16 -0400

    I dunno wtf to do man

--- windows/window.c
+++ windows/window.c
@@ -4265,6 +4265,7 @@ static int TranslateKey(UINT message, WPARAM wParam, LPARAM lParam,
 	}
 	else if (wParam == VK_BACK) {	/* Ctrl Backspace or Ctrl Shift Backspace */
 	    /* We do the opposite of what is configured */
+		MessageBox(NULL, "backspace pressed with shift_state > 1", "weird", 0);
 	    *p++ = (cfg.bksp_is_delete ? 0x08 : 0x7F);
 	    *p++ = 0;
 	    return -2;
